58a614e4a842134f81555d3ce71eafb0b4e77640
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-07-16 16:28:49 +0700

    update: remove border for development

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,13 @@
         <div class="container">
             <div class="text-bg">
                 <h2>DEVELOPER</h2>
+                <h3>Radya.</h3>
                 <div class="bg-1"></div>
                 <div class="bg-2">
                     <img :src="state.img.homeDefault" :class="{ 'img-bounce': state.img.bounceImgDefault }" alt=""
                         @mouseover="handleMouseOver(), play()" @mouseleave="handleMouseLeave(), stop()">
-                    <div class="dialog">
-                        <p>Halo perkenalkan aku muhammad radya iftikhar</p>
+                    <div class="dialog-right" v-if="state.dialog">
+                        <p>{{state.dialog}}</p>
                     </div>
                 </div>
                 <div class="bg-3"></div>
@@ -21,7 +22,7 @@
 import popUp1 from '../assets/sound/popUp1.mp3'
 import popIcon from '../assets/sound/popUp2.mp3'
 
-import homeDefaultImg from '../assets/img/homeBingung.png'
+import homeDefaultImg from '../assets/img/homeDefault.png'
 import homeHoverImg from '../assets/img/homeHover.png'
 
 import { useSound } from '@vueuse/sound'
@@ -39,6 +40,7 @@ useHead({
 
 const state = reactive({
     title: "Radya",
+    dialog: null,
     img: {
         bounceImgDefault: false,
         homeDefault: homeDefaultImg
@@ -50,11 +52,13 @@ const { play, stop } = useSound(popUp1)
 function handleMouseOver() {
     state.img.homeDefault = homeHoverImg
     state.img.bounceImgDefault = true
+    state.dialog = "Hi aku Radya!"
 }
 
 function handleMouseLeave() {
     state.img.homeDefault = homeDefaultImg
     state.img.bounceImgDefault = false
+    state.dialog = null
 }
 
 
