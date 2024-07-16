af386739e2983f3a36550eb31bc35e810f21735b
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-07-16 22:24:10 +0700

    add: img about me

---
+++ components/loading.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="bg"></div>
+    </div>
+</template>
+
+<style scoped>
+.bg {
+    z-index: 999;
+    width: 100wh;
+    height: 100vh;
+    background-color: white;
+    margin-left: 1300px;
+    animation: slide 1s;
+}
+
+@keyframes slide {
+    0% {
+        margin-left: 0px;
+    }
+
+    50% {
+        margin-left: 0px;
+    }
+
+    100% {
+        margin-left: 1300px;
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,32 @@
 <template>
     <div>
         <div class="container">
-            <div class="text-bg">
-                <h2>DEVELOPER</h2>
-                <h3>Radya.</h3>
-                <div class="bg-1"></div>
-                <div class="bg-2">
-                    <img :src="state.img.homeDefault" :class="{ 'img-bounce': state.img.bounceImgDefault }" alt=""
-                        @mouseover="handleMouseOver(), play()" @mouseleave="handleMouseLeave(), stop()">
-                    <div class="dialog-right" v-if="state.dialog">
-                        <p>{{state.dialog}}</p>
+            <main>
+                <Loading v-if="!state.stopSlide" />
+                <div class="text-bg">
+                    <h2>DEVELOPER</h2>
+                    <h3>Radya.</h3>
+                    <div class="bg-1">
+                        <div>
+                            <img :src="state.img.homeAboutMe" alt="about me" @mouseover="handleMouseOver('About me?'), play()" @mouseleave="handleMouseLeave(), stop()">
+                        </div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="bg-2" v-if="state.stopSlide">
+                        <img :src="state.img.homeDefault" :class="{ 'img-bounce': state.img.bounceImgDefault }" alt=""
+                            @mouseover="handleMouseOver('Hi! Aku Radya'), play()" @mouseleave="handleMouseLeave(), stop()">
+                        <div class="dialog-right" v-if="state.dialog">
+                            <p>{{ state.dialog }}</p>
+                        </div>
+                    </div>
+                    <div class="bg-3">
+                        <div></div>
+                        <div></div>
+                        <div></div>
                     </div>
                 </div>
-                <div class="bg-3"></div>
-            </div>
+            </main>
         </div>
     </div>
 </template>
@@ -24,9 +37,11 @@ import popIcon from '@/assets/sound/popUp2.mp3'
 
 import homeDefaultImg from '@/assets/img/homeDefault.png'
 import homeHoverImg from '@/assets/img/homeHover.png'
+import homeAboutMe from '@/assets/img/homeAboutMe.png'
 
 import { useSound } from '@vueuse/sound'
-import { reactive } from 'vue';
+import { onMounted, reactive } from 'vue';
+import Loading from '../components/loading.vue'
 
 useHead({
     title: 'Radya - Home',
@@ -43,16 +58,17 @@ const state = reactive({
     dialog: null,
     img: {
         bounceImgDefault: false,
-        homeDefault: homeDefaultImg
+        homeDefault: homeDefaultImg,
+        homeAboutMe: homeAboutMe
     }
 })
 
 const { play, stop } = useSound(popUp1)
 
-function handleMouseOver() {
+function handleMouseOver(msg) {
     state.img.homeDefault = homeHoverImg
     state.img.bounceImgDefault = true
-    state.dialog = "Hi aku Radya!"
+    state.dialog = msg
 }
 
 function handleMouseLeave() {
@@ -61,7 +77,11 @@ function handleMouseLeave() {
     state.dialog = null
 }
 
-
+onMounted(() => {
+    setTimeout(() => {
+        state.stopSlide = true
+    }, 1200);
+})
 
 </script>
 
